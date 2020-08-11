a9327b43b447c41a8f4f6af4cb1dcfd39866d45b
erika santana <erika.araujo12344@gmail.com>
2020-08-11 17:32:25 -0300

    incluindo Bootstrap-4

---
+++ form.js
@@ -0,0 +1,9 @@
+function Enviar() {
+
+    var nome = document.getElementById("nomeid");
+    var fone = document.getElementById("foneid");
+    var email = document.getElementById("emailid");
+
+    console.log(`nome -> ${nome.value}, fone -> ${fone.value}, email -> ${email.value}`)
+
+}
